34cab3c5f31ba3f6d111ea7c356f166a88eff957
Rene <rene.roets@outlook.com>
2019-12-12 16:31:21 +0100

    iframe chatbot design

---
+++ chatbot website/chatbot.js
@@ -0,0 +1,8 @@
+    function toShow() {
+        var iframe = document.getElementById("frame1");
+            if (iframe.style.display === "none") {
+                iframe.style.display = "block";
+         } else {
+        iframe.style.display = "none";
+            }
+        }
